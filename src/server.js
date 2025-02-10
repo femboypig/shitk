5f68574471fa77181dbbb1bef1fe2787d30633cb
femboyPig <harimassaa@gmail.com>
2025-02-10 16:58:46 +0300

    апввапва

--- src/server.js
+++ src/server.js
@@ -4,26 +4,32 @@ const cors = require('cors');
 const axios = require('axios');
 const admin = require('firebase-admin');
 
+// Проверяем наличие переменной окружения
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+    // Если переменной нет, используем локальный файл
+    const serviceAccount = require('./serviceAccountKey.json');
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+} else {
+    // Используем переменную окружения
+    admin.initializeApp({
+        credential: admin.credential.cert({
+            "type": "service_account",
+            "project_id": "shitk-p",
+            "private_key_id": "87d887ca3ad9fe290666d4a371bdca83fcaec4b2",
+            "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            "client_email": "firebase-adminsdk-fbsvc@shitk-p.iam.gserviceaccount.com",
+            "client_id": "109874227719706049138",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40shitk-p.iam.gserviceaccount.com"
+        })
+    });
+}
 
-// Инициализация Firebase Admin SDK
-admin.initializeApp({
-    credential: admin.credential.cert({
-        "type": "service_account",
-        "project_id": "shitk-p", 
-        "private_key_id": "87d887ca3ad9fe290666d4a371bdca83fcaec4b2",
-        "private_key": process.env.FIREBASE_PRIVATE_KEY 
-            ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-            : undefined,
-        "client_email": "firebase-adminsdk-fbsvc@shitk-p.iam.gserviceaccount.com",
-        "client_id": "109874227719706049138",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth", 
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40shitk-p.iam.gserviceaccount.com"
-    }),
-    databaseURL: "https://shitk-p-default-rtdb.firebaseio.com/"
-});
-
+// Инициализируем Firestore
 const db = admin.firestore();
 
 // Initialize express app
