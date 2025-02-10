d9d22ec57b141fb4c87e196c29b54df3784fdfd4
femboyPig <harimassaa@gmail.com>
2025-02-10 16:54:30 +0300

    е

--- src/server.js
+++ src/server.js
@@ -3,11 +3,24 @@ const path = require('path');
 const cors = require('cors');
 const axios = require('axios');
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+
 
 // Инициализация Firebase Admin SDK
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "shitk-p", 
+        "private_key_id": "87d887ca3ad9fe290666d4a371bdca83fcaec4b2",
+        "private_key": process.env.FIREBASE_PRIVATE_KEY 
+            ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+            : undefined,
+        "client_email": "firebase-adminsdk-fbsvc@shitk-p.iam.gserviceaccount.com",
+        "client_id": "109874227719706049138",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth", 
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40shitk-p.iam.gserviceaccount.com"
+    }),
     databaseURL: "https://shitk-p-default-rtdb.firebaseio.com/"
 });
 
