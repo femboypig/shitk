d91c8a82387392c970fb8503b90a06a28e37f00d
femboyPig <harimassaa@gmail.com>
2025-02-10 17:12:00 +0300

    \sda

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const initializeFirebaseAdmin = () => {
       // For production, use environment variables
       const serviceAccount = {
         type: "service_account",
-        project_id: process.env.FIREBASE_PROJECT_ID,
+        project_id: "shitk-p",
         private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
         private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         client_email: process.env.FIREBASE_CLIENT_EMAIL,
@@ -24,7 +24,7 @@ const initializeFirebaseAdmin = () => {
 
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        databaseURL: process.env.FIREBASE_DATABASE_URL
+        databaseURL: "https://shitk-p-default-rtdb.firebaseio.com"
       });
     } else {
       // For development, use local service account file
@@ -39,6 +39,9 @@ const initializeFirebaseAdmin = () => {
     throw error;
   }
 };
+
+// Call the initialization function immediately
+initializeFirebaseAdmin();
 
 // Инициализируем Firestore сразу после инициализации админа
 const db = admin.firestore();
